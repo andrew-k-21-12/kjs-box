b368de47a82d5f133e0645f92bd91c55e2681a6a
Andrey Koshmanskiy <andrew.k.21.12@gmail.com>
2022-06-24 19:43:15 +0300

    Kotlin -> 1.7.0, dependencies are refreshed

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,21 +15,21 @@ dependencies {
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react:${Configs.REACT_VERSION}-${Configs.KOTLIN_WRAPPER_VERSION}")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-redux:7.2.6-${Configs.KOTLIN_WRAPPER_VERSION}")      // to use global state in React
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-router-dom:6.3.0-${Configs.KOTLIN_WRAPPER_VERSION}") // to process routes
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-styled-next:1.1.0-${Configs.KOTLIN_WRAPPER_VERSION}")      // to declare and reuse styles directly in code
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-styled-next:1.2.1-${Configs.KOTLIN_WRAPPER_VERSION}")      // to declare and reuse styles directly in code
 
     // Dependency injection.
-    implementation("org.kodein.di:kodein-di:7.10.0")
+    implementation("org.kodein.di:kodein-di:7.13.0")
 
     // Localization.
-    implementation(npm("i18next", "21.6.11"))
-    implementation(npm("react-i18next", "11.15.4"))
-    implementation(npm("i18next-browser-languagedetector", "6.1.3"))
-    implementation(npm("i18next-http-backend", "1.3.2")) // to download translations on demand
+    implementation(npm("i18next", "21.8.10"))
+    implementation(npm("react-i18next", "11.17.2"))
+    implementation(npm("i18next-browser-languagedetector", "6.1.4"))
+    implementation(npm("i18next-http-backend", "1.4.1")) // to download translations on demand
 
     // Bundling.
     implementation(devNpm("html-webpack-plugin", "5.5.0"))
     implementation(devNpm("uglifyjs-webpack-plugin", "2.2.0"))
-    implementation(devNpm("terser-webpack-plugin", "5.3.1"))
+    implementation(devNpm("terser-webpack-plugin", "5.3.3"))
     implementation(devNpm("copy-webpack-plugin", "9.1.0" )) // newer versions don't work correctly with npm and Yarn
     implementation(devNpm("node-json-minify", "3.0.0"))
     implementation(devNpm("@svgr/webpack", "6.2.1"))
@@ -48,5 +48,8 @@ kotlin {
 
 // An Apple Silicon compilation fix.
 rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
-    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().nodeVersion = "16.0.0"
+    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().apply {
+        nodeVersion                 = "16.0.0"
+        versions.webpackCli.version = "4.10.0"
+    }
 }
--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -2,7 +2,7 @@
  * All buildscript-related configs to avoid hardcoding and duplication.
  * */
 object Configs {
-    const val KOTLIN_VERSION         = "1.6.21"
-    const val KOTLIN_WRAPPER_VERSION = "pre.332-kotlin-$KOTLIN_VERSION"
-    const val REACT_VERSION          = "18.0.0"
+    const val KOTLIN_VERSION         = "1.7.0"
+    const val KOTLIN_WRAPPER_VERSION = "pre.347"
+    const val REACT_VERSION          = "18.2.0"
 }
